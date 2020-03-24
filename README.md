78bed0178a464bd4372ae1451885faaf61298a68
Jon Hill <jondhill1984@gmail.com>
2020-03-24 18:23:10 +0200

    final logic, cleaned up codes and removed lite server from start scripts, ready for heroku

---
+++ README.md
@@ -0,0 +1,38 @@
+1.    Title — What isyour app/project called?
+
+Matching Game
+
+2.    Overview — Why didyou start this project?
+
+This project was taken on as my solo project for my firstchingu voyage. Its tier 2. I chose this and it seemed like a good way to put theskills I have learned into practice the skills with a simple, interactiveapplication
+
+
+3.    Features — Whatare some key things your project can do?
+
+Players can click or tap on each card to flip it over.There are 8 pairs of matching cards. If the cards match they stay flipped over,if they aren’t they flip back. Once the player has discovered all 8 pairs, theyget the “YOU WON” Message
+
+4.    Running theproject — How could someone else get your code working for them?
+
+You should be fairly easy, just clone the repo fromGithub, cd into the correct folder and npm start (or yarn) and that’s it.
+
+5.    Dependencies —What are the main outside resources your project needs to run?
+
+Font-awesome
+
+6 . To do list
+
+- At the time of submission on 24.03, I plan to make thefollowing adjustments:
+
+- Refactor the code to be cleaner
+
+- Update the code comments to be clearer
+
+- Re-vamp the styles – colors, icons, fonts etc
+
+Also further extensions:
+
+- Build space to the side, where matched cards slide to oncethey are matched, to practice      implementing more css animations
+
+- Implement move counterImplement star rating for how quickly the player isgetting to matching all pairs
+
+- Create a whole new version of the app using React - to practice React
---
+++ scripts.js
@@ -0,0 +1,93 @@
+
+    let hasFlippedCard = false;
+    let lockBoard = false;
+    let firstCard, secondCard;
+    let score = 0;
+        
+    let cards = document.querySelectorAll('.cardContainer');
+        
+    const resetButton = document.getElementById("reset");
+        
+    let wonMessage = document.getElementById("wonMessage");
+
+    function flipCard() {
+        if (lockBoard) return;
+        if (this === firstCard) return;
+        
+        this.classList.add("flip")
+        
+        // if its the first card, have another go
+        if(!hasFlippedCard) {
+            hasFlippedCard = true;
+            firstCard = this;
+        } else {
+            hasFlippedCard = false;
+            secondCard = this;
+        // check for a match
+            checkMatch()
+        }
+         //if match then       
+        function checkMatch() {
+            if(firstCard.dataset.name === secondCard.dataset.name){
+            // leave the cards turned over
+                matchedCards()
+//             update the score
+                score++
+                console.log(score);
+
+            } else {
+        // if no match turn back over
+                unflipCards();
+            }
+
+        }
+        
+        function matchedCards() {
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+            
+        }
+        
+        function unflipCards() {
+            lockBoard = true;
+            
+            setTimeout(() => {
+            firstCard.classList.remove("flip");
+            secondCard.classList.remove("flip");
+                
+            newTurn()
+            }, 1600);
+         }
+        
+        if(score === 8){
+            console.log("working");
+            wonMessage.innerHTML = `YOU WON!!!`;
+            }
+        
+    };
+        
+        function newTurn() {
+            hasFlippedCard = false;
+            lockBoard = false;
+            firstCard = null;
+            secondCard = null;
+        }
+          
+        function newGame() {
+            cards.forEach(card => {
+                let mixedCards = Math.floor(Math.random() * 12);
+                card.style.order = mixedCards;
+                });
+            cards.forEach(x => {
+               x.classList.remove("flip"); 
+            })
+            hasFlippedCard = false;
+            lockBoard = false;
+            firstCard = null;
+            secondCard = null;
+        };        
+            
+    cards.forEach(card => card.addEventListener('click', flipCard));
+        
+    resetButton.addEventListener("click", newGame);
+        
